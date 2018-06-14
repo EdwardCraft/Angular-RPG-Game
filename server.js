1f50a26f2625fc981a1777237b93f02f2e49b6a0
EdwardCraft <eduardo.flores81@uabc.edu.mx>
2018-06-14 09:05:45 -0700

    test

--- server.js
+++ server.js
@@ -5,14 +5,18 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist'));
+app.set('port', (process.env.PORT || 8080));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/index.html'));
+//app.use(express.static(__dirname + '/dist'));
+app.use(express.static(path.resolve(__dirname,'src')))
+
+app.get('/', function(req,res) {   
+	res.sendFile(path.join(__dirname+'/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
 
-console.log('server started!');
+app.listen(app.get('port'), function(){
+	console.log('Node app is running on port', app.get('port'));
+});
